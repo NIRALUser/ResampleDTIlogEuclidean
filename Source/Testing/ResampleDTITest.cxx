0742e96d312469d8dbb33d40900064f22fb01a06
adrienkaiser <akaiser@unc.edu>
2013-01-31 16:53:08 -0500

    COMP: Windows: If ResampleDTIlogEuclideanLib compiled statically, test executables tried to link with dynamic lib (dll) whereas the lib is static: remove dllimport

--- Source/Testing/ResampleDTITest.cxx
+++ Source/Testing/ResampleDTITest.cxx
@@ -1,7 +1,7 @@
 #include "itkTestMainExtended.h"
 
 #ifdef WIN32
-#define MODULE_IMPORT __declspec(dllimport)
+#define MODULE_IMPORT// __declspec(dllimport) // "dllimport" -> Dll = Dynamic library on windows -> if ResampleDTIlogEuclidean is static, just set MODULE_IMPORT to nothing: Fails to link (linkage error) if dllimport because library is static and tries to link as dynamic
 #else
 #define MODULE_IMPORT
 #endif
