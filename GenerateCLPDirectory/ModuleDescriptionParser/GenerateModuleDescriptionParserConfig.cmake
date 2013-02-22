6132ced1541470a47fc0efceee1534e683dda1a4
Hans Johnson <hans-johnson@uiowa.edu>
2013-02-21 21:04:57 -0600

    STYLE: Convert CMake-language commands to lower case
    Ancient CMake versions required upper-case commands.  Later
    command names became case-insensitive.  Now the preferred style
    is lower-case.
    This also changes all the key words to lower case. The
    primary reason for changing key words is that all
    documentation for CMakeLists.txt now shows the key words as
    lower case. Even the printed “Mastering CMake v5” uses lower
    case.

--- GenerateCLPDirectory/ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
+++ GenerateCLPDirectory/ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
@@ -1,17 +1,17 @@
-# Generate the ModuleDescriptionParserConfig.cmake file in the build tree 
+# Generate the ModuleDescriptionParserConfig.cmake file in the build tree
 # and configure one the installation tree.
 
 # Settings specific to build trees
 #
 #
-set(ModuleDescriptionParser_INCLUDE_DIRS_CONFIG 
-  ${ModuleDescriptionParser_BINARY_DIR} 
+set(ModuleDescriptionParser_INCLUDE_DIRS_CONFIG
+  ${ModuleDescriptionParser_BINARY_DIR}
   ${ModuleDescriptionParser_SOURCE_DIR}
   )
-set(ModuleDescriptionParser_LIBRARY_DIRS_CONFIG 
+set(ModuleDescriptionParser_LIBRARY_DIRS_CONFIG
   ${ModuleDescriptionParser_BINARY_DIR}
   )
-set(ModuleDescriptionParser_USE_FILE_CONFIG 
+set(ModuleDescriptionParser_USE_FILE_CONFIG
   ${ModuleDescriptionParser_BINARY_DIR}/UseModuleDescriptionParser.cmake
   )
 set(ITK_DIR_CONFIG ${ITK_DIR})
@@ -21,7 +21,7 @@ set(ITK_DIR_CONFIG ${ITK_DIR})
 #
 configure_file(
   ${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserConfig.cmake.in
-  ${ModuleDescriptionParser_BINARY_DIR}/ModuleDescriptionParserConfig.cmake 
+  ${ModuleDescriptionParser_BINARY_DIR}/ModuleDescriptionParserConfig.cmake
   @ONLY IMMEDIATE
   )
 
@@ -33,6 +33,6 @@ configure_file(
 #
 configure_file(
   ${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserInstallConfig.cmake.in
-  ${ModuleDescriptionParser_BINARY_DIR}/install/ModuleDescriptionParserConfig.cmake 
+  ${ModuleDescriptionParser_BINARY_DIR}/install/ModuleDescriptionParserConfig.cmake
   @ONLY IMMEDIATE
   )
